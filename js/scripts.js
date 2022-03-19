1fe7780c945ec887cedccdd8befe4d521bc2910f
claudia <xclaudiaarias@gmail.com>
2022-03-19 00:59:51 -0400

    Initialized repo. Added Files. Starter Code.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let gameContainer = $("game-container");
+const startGameBtn = $("start-game");
+const playAgain = $("play-again-btn");
+
+const imgs_num = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg"];
+
+let score = 0;
+let level = 0;
+let isGame = true;
+
+// functions 
+
