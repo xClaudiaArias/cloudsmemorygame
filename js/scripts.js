5b5b9db6064cd847e97f7d719ae55e6ebfd03acb
claudia <xclaudiaarias@gmail.com>
2022-03-19 02:55:13 -0400

    added functions and more variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,48 @@
-let gameContainer = $("game-container");
-const startGameBtn = $("start-game");
-const playAgain = $("play-again-btn");
+let gameContainer = $("#game-container");
+const startGameBtn = $("#start-game");
+const playAgain = $("#play-again-btn");
 
-const imgs_num = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg"];
+let allImgs = ["1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg"];
+// images per level 
+let imgs = [];
+
+console.log(imgs, ' images BEFORE')
 
 let score = 0;
 let level = 0;
 let isGame = true;
 
+// this is how many images will be displayed per level
+let nums_of_imgs = 0;
+
+
 // functions 
+const start_game = () => {
+    nums_of_imgs = 4;
+
+
+};
+
+const updateLevel = () => {
+    return level += 1;
+};
+
+const updateScore = () => {
+    return score += 1;
+};
+
+const generateImgs = () => {
+    if (isGame){
+        if (level === 1) {
+            
+        }
+    }
+
+};
+
+const winnerModal = () => {
+
+}
+
+
 
